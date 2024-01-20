324b1a1ef6596a5a51ec07199df776573bf6d7bd
Chad Edwards <chadedwardsofficial@gmail.com>
2024-01-20 09:36:30 -0600

    added schema only for Reactions within the Thoughts model

--- models/Thought.js
+++ models/Thought.js
@@ -23,6 +23,29 @@ thoughtSchema.virtual('reactionCount').get(function(){
     return this.reactions.length;
 })
 
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Schema.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+
+
 
 const Thought = model('Thought', thoughtSchema);
 
